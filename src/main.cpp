f10db10285a0d0bb03e01533c34ac0ac023a7313
pyu <1391989896@qq.com>
2025-11-06 23:10:40 +0800

    loop command

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    std::string command;
-    std::cout << "$ ";  // 初始提示符
+    std::string line;
+    std::cout << "$ "; // 初始提示符
 
-    while (std::getline(std::cin, command)) {
-        if (command == "exit") {
-            return 0;
+    while (std::getline(std::cin, line)) {
+        std::istringstream iss(line);
+        std::string cmd;
+        std::string arg;
+
+        //解析出第一个和第二个单词
+        iss >> cmd >> arg;
+
+        if (cmd == "exit") {
+            int code = 0;
+            if (!arg.empty()) {
+                code = std::stoi(arg);
+            }
+            return code;
         }
 
-        std::cout << command << ": command not found" << std::endl;
+        std::cout << cmd << ": command not found" << std::endl;
         std::cout << "$ ";
     }
 
