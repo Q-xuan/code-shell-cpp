5c0bbc0983858c970bc49caaae11706b79c93c38
pyu <1391989896@qq.com>
2025-11-06 22:41:08 +0800

    handle invalid command

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,19 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
+
+    std::string command;
+    //读取内容
+    std::getline(std::cin, command);
+
+    if (command != "echo" || command != "cd") {
+        std::cout << command << ": command not found" << std::endl;
+        return 1;
+    }
 }
