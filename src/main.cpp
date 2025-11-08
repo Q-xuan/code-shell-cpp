e81ad890a35eb857f8192559f6cae856c3c3143a
pyu <1391989896@qq.com>
2025-11-08 20:45:18 +0800

    impl Locate executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
+#include <filesystem>
 #include <functional>
 #include <iostream>
 #include <map>
 #include <string>
 #include <sstream>
 #include <vector>
+
+//查询文件是否存在并且是否是可执行的
+bool isExecutable(const std::string &path) {
+    const std::filesystem::path filePath(path);
+    return std::filesystem::exists(filePath) && std::filesystem::is_regular_file(filePath) && (
+               std::filesystem::status(filePath).permissions() & std::filesystem::perms::owner_exec) !=
+           std::filesystem::perms::none;
+}
+
+// 查找命令
+void findCommandPath(const std::string &command) {
+    if (const char *path = std::getenv("PATH")) {
+        const std::string path_str = path;
+        std::stringstream ss(path_str);
+        std::string token;
+
+        // 遍历 PATH 中的每个目录
+        while (std::getline(ss, token, ':')) {
+            std::string command_path = token + "/" + command;
+            if (isExecutable(command_path)) {
+                std::cout << command << " is " << command_path << std::endl;
+                return; // 找到命令后返回
+            }
+        }
+    }
+    std::cout << command << ": not found" << std::endl;
+}
 
 // 去掉前导空格
 void ltrim(std::string &s) {
@@ -27,7 +55,7 @@ int exitMethod(const std::string &arg) {
 }
 
 int typeMethod(const std::string &arg,
-               const std::map<std::string, std::function<int(const std::string &)> >& commands) {
+               const std::map<std::string, std::function<int(const std::string &)> > &commands) {
     if (const auto it = commands.find(arg); it == commands.end()) {
         std::cout << arg << ": not found" << std::endl;
         return -1;
@@ -72,7 +100,7 @@ int main() {
                 return code;
             }
         } else {
-            std::cout << cmd << ": command not found" << std::endl;
+            findCommandPath(cmd);
         }
 
 
